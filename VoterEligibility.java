5a489eef31b4923c5fe7b07dfc84415573cbbfd0
Rohan Sawant <74293728+rohansawant735@users.noreply.github.com>
2022-02-02 13:42:38 +0530

    Add files via upload

---
+++ VoterEligibility.java
@@ -0,0 +1,10 @@
+public class VoterEligibility{
+public static void main(String[] args){
+int age = 22;
+if(age<18){
+System.out.println("You ain't eligible.");
+} else {
+System.out.println("Choose your next leader.you're eligible");
+}
+}
+}
